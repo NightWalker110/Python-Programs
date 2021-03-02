2a3c8b0d10de4c1475783e1edd4d606dd8ec2dec
Shubham Kumar Singh <61842432+NightWalker110@users.noreply.github.com>
2021-03-02 22:45:54 +0530

    Create Cow.py

---
+++ Cowsay animals/Cow.py
@@ -0,0 +1,13 @@
+import cowsay
+cowsay.cow("hello")
+
+___
+< hello >
+  =====
+          \
+           \
+             ^__^                             
+             (oo)\___                   
+             (__)\       )\/\             
+                 ||----w |           
+                 ||     ||
