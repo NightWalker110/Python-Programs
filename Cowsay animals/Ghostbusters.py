74377f487f27af139d0e4454006dd35780db185a
Shubham Kumar Singh <61842432+NightWalker110@users.noreply.github.com>
2021-03-02 22:51:30 +0530

    Create Ghostbusters.py

---
+++ Cowsay animals/Ghostbusters.py
@@ -0,0 +1,29 @@
+import cowsay
+cowsay.ghostbusters("hello")
+
+  ___
+< hello >
+  =====
+          \
+           \
+            \
+             \   
+                         _---_
+                      -       /--___
+                 __--( /     \ )XXXXXXXXXXX\v.
+               .-XXX(   O   O  )XXXXXXXXXXXXXXX-
+              /XXX(       U     )        XXXXXXX\
+            /XXXXX(              )--_  XXXXXXXXXXX\
+           /XXXXX/ (      O     )   XXXXXX   \XXXXX\
+           XXXXX/   /            XXXXXX   \__ \XXXXX
+           XXXXXX_/          XXXXXX         \_---->
+   ---_  XXX_/          XXXXXX      \_         /
+     \-  --_/   _/\  XXXXXX            /  __--/=
+      \-\    _/    XXXXXX              '--- XXXXXX
+         \-\/XXX\ XXXXXX                      /XXXXX
+           \XXXXXXXXX   \                    /XXXXX/
+            \XXXXXX      >                 _/XXXXX/
+              \XXXXX--_/              _-- XXXX/
+               -XXXXXXXX---------------  XXXXXX-
+                  \XXXXXXXXXXXXXXXXXXXXXXXXXX/
+                    ""VXXXXXXXXXXXXXXXXXXV""
