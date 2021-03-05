e7fbbd3d606a6b81468994f3f2177718dd3dd7bf
Shubham Kumar Singh <61842432+NightWalker110@users.noreply.github.com>
2021-03-05 23:36:30 +0530

    Create Calendar.py

---
+++ Miscellaneous/Calendar.py
@@ -0,0 +1,18 @@
+import calendar
+year = int(input("enter year: "))
+month = int(input("enter month: "))
+print(calendar.month(year,month))
+
+//
+Output : 
+enter year: 2021
+enter month: 8
+    August 2021
+Mo Tu We Th Fr Sa Su
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30 31
+//
